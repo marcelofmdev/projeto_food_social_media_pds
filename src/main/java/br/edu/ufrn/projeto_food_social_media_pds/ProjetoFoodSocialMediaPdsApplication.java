d583178da2273f84ed9b9822d4c481acbf6df90c
Marcelo Ferreira <marojamf@gmail.com>
2021-11-11 20:02:57 -0300

    Projeto gerado

---
+++ src/main/java/br/edu/ufrn/projeto_food_social_media_pds/ProjetoFoodSocialMediaPdsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.projeto_food_social_media_pds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFoodSocialMediaPdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFoodSocialMediaPdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufrn/projeto_food_social_media_pds/ProjetoFoodSocialMediaPdsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.projeto_food_social_media_pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFoodSocialMediaPdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
