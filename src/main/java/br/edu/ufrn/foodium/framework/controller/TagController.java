f63f0576a00d56fa0f3b8f81c1126617d622d108
Artur Santiago <artursantiago2001@hotmail.com>
2022-02-14 23:39:12 -0300

    feat: adds cros origin anotation

--- src/main/java/br/edu/ufrn/foodium/framework/controller/TagController.java
+++ src/main/java/br/edu/ufrn/foodium/framework/controller/TagController.java
@@ -3,12 +3,14 @@ package br.edu.ufrn.foodium.framework.controller;
 import br.edu.ufrn.foodium.framework.domain.model.Tag;
 import br.edu.ufrn.foodium.framework.domain.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/tag")
 public class TagController {
